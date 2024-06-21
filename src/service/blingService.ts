e8c52d873d9ebb445329dcebeb69a79ff2f7d36f
Lucas Eleutério Bueno <lucasbueno@frwk.com.br>
2024-06-21 10:08:18 -0300

    Correção env

--- src/service/blingService.ts
+++ src/service/blingService.ts
@@ -11,14 +11,15 @@ export class BlingService {
             url: url,
             headers: {
                 'Accept': 'application/json',
-                'Authorization': 'Bearer fbe3514e347ec29cf3c0bdbae0c7637571b0208c',
-                'Cookie': 'PHPSESSID=i6a2fh9ru2ssfsc8q8vu4fnslv'
+                'Authorization': 'Bearer ' + process.env.BLING_API_KEY,
+                'Cookie': 'PHPSESSID=q2e27dc8qcds7ui2fvqmmc0i1l'
             }
         };
     }
 
     async getProdutos(page: number = 1, limit: number = 10) {
         const config = this.getAxiosConfig(`https://bling.com.br/Api/v3/produtos?pagina=${page}&limite=${limit}`);
+        console.log(config)
         return await axios.request(config);
     }
 
